c8eead6c585d18a2e700835e03f24dd4c852aceb
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-10-21 21:57:49 +0500

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit e0e7b944fd34cb7a94b0791d20d13430cc72c5c4, reversing
    changes made to 1b60d89b3eb098c6858f6fcd8d46ed5a3a30549e.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
 gem update --system
 bundle install
 rails db:prepare
-=======
-mkdir -p .ruby-lsp
-cp .ruby-version .ruby-lsp/.ruby-version
-
-test -f .env || cp .env.example .env
-
-gem update --system
-bundle
-yarn
-
-rails db:create db:migrate db:seed
->>>>>>> origin/main
